cc7fae9724b3f55a3c9bdda4cf841ee51e3f607e
ThreadK <155293762+ThreadK@users.noreply.github.com>
2023-05-13 07:45:20 +0000

    Minor changes and fixes

--- src/caffe/layers/apl_layer.cpp
+++ src/caffe/layers/apl_layer.cpp
@@ -1,1 +1,8 @@
-// Copyright 2014
+// Copyright 2014 BVLC and contributors.
+
+#include <vector>
+
+#include "caffe/blob.hpp"
+#include "caffe/common.hpp"
+#include "caffe/filler.hpp"
+#include "caffe/layer.hpp
