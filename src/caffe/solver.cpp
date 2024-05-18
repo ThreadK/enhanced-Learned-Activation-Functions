943049dc7dd084a1eb7d4ef3e5c02260f9734a94
ThreadK <155293762+ThreadK@users.noreply.github.com>
2024-05-18 21:50:16 +0000

    format-translation

---
+++ src/caffe/solver.cpp
@@ -0,0 +1,33 @@
+
+#include <cstdio>
+
+#include <algorithm>
+#include <string>
+#include <vector>
+
+#include "caffe/net.hpp"
+#include "caffe/proto/caffe.pb.h"
+#include "caffe/solver.hpp"
+#include "caffe/util/io.hpp"
+#include "caffe/util/math_functions.hpp"
+#include "caffe/util/upgrade_proto.hpp"
+
+namespace caffe {
+
+template <typename Dtype>
+Solver<Dtype>::Solver(const SolverParameter& param)
+    : net_() {
+  Init(param);
+}
+
+template <typename Dtype>
+Solver<Dtype>::Solver(const string& param_file)
+    : net_() {
+  SolverParameter param;
+  ReadProtoFromTextFileOrDie(param_file, &param);
+  Init(param);
+}
+
+template <typename Dtype>
+void Solver<Dtype>::Init(const SolverParameter& param) {
+  LOG(INFO) << "Initializing solver from parameters: " << std::endl
